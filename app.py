a903f706c7180ed5de64c1fdac1f919cefca457b
Parne92 <nathan_parnell3@hotmail.com>
2024-05-30 23:24:22 -0700

    cleaning up unused imports

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-import os
 from datetime import datetime
 
-from flask import Flask, Blueprint, redirect, render_template, request, send_from_directory, url_for, flash
-from flask_migrate import Migrate
+from flask import Flask, redirect, render_template, request, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Float
-from sqlalchemy.orm import validates
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_wtf.csrf import CSRFProtect
-from flask_login import UserMixin, LoginManager, login_user, login_required, current_user, logout_user
+from flask_login import LoginManager, login_user, login_required, current_user, logout_user
 
 
 app = Flask(__name__, static_folder='static')
@@ -21,33 +17,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 # Initialize the database connection
 db = SQLAlchemy(app)
 
-#from models import Player, Game
-
-class Player(UserMixin, db.Model):
-    __tablename__ = 'player'
-    id = db.Column(Integer, primary_key=True)
-    username = db.Column(String(20), unique = True)
-    password = db.Column(db.String(100))
-    rating = db.Column(Float)
-
-    def __str__(self):
-        return self.name
-
-class Game(db.Model):
-    __tablename__ = 'game'
-    id = db.Column(Integer, primary_key = True)
-    position = db.Column(String(10))
-    playerid = Column(Integer, ForeignKey('player.id', ondelete="CASCADE"))
-    rating = Column(Float)
-    game_date = Column(DateTime)
-    goals = Column(Integer)
-    assists = Column(Integer)
-    tackles = Column(Integer)
-    saves = Column(Integer)
-    time = Column(Integer)
-    
-    def __str__(self):
-        return f"{self.player}, {self.position}: {self.rating} ON {self.game_date:%x}"
+from models import Player, Game
 
 with app.app_context():
     db.create_all()
--- models.py
+++ models.py
@@ -1,9 +1,10 @@
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Float
 from sqlalchemy.orm import validates
+from flask_login import UserMixin
 
 from app import db
 
-class Player(db.Model):
+class Player(UserMixin, db.Model):
     __tablename__ = 'player'
     id = db.Column(Integer, primary_key=True)
     username = db.Column(String(20), unique = True)
@@ -17,11 +18,14 @@ class Game(db.Model):
     __tablename__ = 'game'
     id = db.Column(Integer, primary_key = True)
     position = db.Column(String(10))
-    player = Column(String(20), ForeignKey('Player.username', ondelete="CASCADE"))
+    playerid = Column(Integer, ForeignKey('player.id', ondelete="CASCADE"))
     rating = Column(Float)
     game_date = Column(DateTime)
+    goals = Column(Integer)
+    assists = Column(Integer)
+    tackles = Column(Integer)
+    saves = Column(Integer)
+    time = Column(Integer)
     
     def __str__(self):
         return f"{self.player}, {self.position}: {self.rating} ON {self.game_date:%x}"
-
-
